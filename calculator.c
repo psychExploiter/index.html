28ecc8d42b1960d4286d5d1d6ddcaf25d897c511
psychExploiter <152595076+psychExploiter@users.noreply.github.com>
2023-12-13 14:28:20 +0530

    Add files via upload
    Signed-off-by: psychExploiter <152595076+psychExploiter@users.noreply.github.com>

--- calculator.c
+++ calculator.c
@@ -4,6 +4,9 @@ int main() {
     int a, b;
     printf("please enter 2 values for additon :- ");
     scanf("%d %d", &a,&b);
-    printf("addition of 2 numbers is= %d", (a+b));
+    printf("addition of 2 numbers is= %d\n", (a+b));
+     printf("please enter 2 values for division :- ");
+     scanf("%d %d", &a,&b);
+    printf("division of 2 numbers is= %d", (a/b));
     return 0;
 }
