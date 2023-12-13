061eae7f83aa01b00a94b5b87a79b41074f12f24
psychExploiter <152595076+psychExploiter@users.noreply.github.com>
2023-12-13 13:35:12 +0530

    Add files via upload
    Signed-off-by: psychExploiter <152595076+psychExploiter@users.noreply.github.com>

---
+++ calculator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("please enter 2 values for additon :- ");
+    scanf("%d %d", &a,&b);
+    printf("addition of 2 numbers is= %d", (a+b));
+    return 0;
+}
